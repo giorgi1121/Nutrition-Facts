b4f54f17b737076f04bb276aa47752edce239cd6
Giorgi Abashidze <126471823+giorgi1121@users.noreply.github.com>
2023-03-15 02:12:08 +0400

    Create nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,29 @@
+#create dictionary fruits
+fruits = {"Apple": 130,
+          "Avocado": 50,
+          "Banana": 110,
+          "Cantaloupe": 50,
+          "Grapefruit": 60,
+          "Grapes": 90,
+          "Honeydew Melon": 50,
+          "Kiwifruit": 90,
+          "Lemon": 15,
+          "Lime": 20,
+          "Nectarine": 60,
+          "Orange": 80,
+          "Peach": 60,
+          "Pear": 100,
+          "Pineapple": 50,
+          "Plums": 70,
+          "Strawberries": 50,
+          "Sweet Cherries": 100,
+          "Tangerine": 50,
+          "Watermelon": 80
+          }
+
+#variable item
+item = str(input("Item: ")).title()
+
+#check if item is in fruits and print its value from dictionary
+if item in fruits:
+    print("Calories: " + str(fruits[item]))
